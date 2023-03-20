e309446a66ab18c40c3a8cbdcd2375517ecbfa44
Marina Safwat <102893216+Marina-Safwat@users.noreply.github.com>
2023-03-21 00:29:34 +0200

    Add files via upload

---
+++ C_Programming/C_Basics/Assignments/1_Print.c
@@ -0,0 +1,18 @@
+/*
+ * main.c
+ *
+ *  Created on: Mar 20, 2023
+ *      Author: Marina
+ *
+EX1:
+
+Write C Program to Print a Sentence
+C Programming
+
+ */
+
+#include <stdio.h>
+int main()
+{
+	printf("C Programming");
+}
